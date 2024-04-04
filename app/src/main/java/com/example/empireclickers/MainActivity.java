3a0d0600f3b6ea1d197cd51be355e3631d5c1e3c
Clarissa Toh <clarissatoh03@gmail.com>
2024-04-04 13:01:47 +0800

    removed extra navigation menu

--- app/src/main/java/com/example/empireclickers/MainActivity.java
+++ app/src/main/java/com/example/empireclickers/MainActivity.java
@@ -281,13 +281,6 @@ public class MainActivity extends AppCompatActivity {
 //    }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.bottom_nav_menu, menu);
-        return true;
-    }
-
-    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.navigation_main:
